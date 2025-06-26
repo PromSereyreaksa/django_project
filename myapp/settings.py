d0bd2538b0df8073bb875e00a0cbf0f0f444b74a
Prom Sereyreaksa <prumsereyreaksa@gmail.com>
2025-06-26 23:01:49 +0700

    allowed host

--- myapp/settings.py
+++ myapp/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-@**r(j+8k3wu)8-4g6^i*oc(u8)+cnyhzhv&1q806&ji3c#g3_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 GOOGLE_OAUTH_CLIENT_ID = os.environ.get('GOOGLE_OAUTH_CLIENT_ID')
 if not GOOGLE_OAUTH_CLIENT_ID:
