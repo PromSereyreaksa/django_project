870cb77ba34856e9406d4a5d8642c52dc42ef1e1
Prom Sereyreaksa <prumsereyreaksa@gmail.com>
2025-06-28 18:24:29 +0700

    Styling

--- accounts/views.py
+++ accounts/views.py
@@ -165,6 +165,16 @@ def profile_views(request):
 
     return render(request, 'profile_av.html', {'profile': profile})
 
+def edit_profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Profile updated successfully.")
+            return redirect('profile')
+    else:
+        form = ProfileForm(instance=request.user.profile)
+    return render(request, 'edit_profile.html', {'form': form, 'title': 'Edit Profile'})
 
 
 
